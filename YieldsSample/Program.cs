c0797cdae62eef0c20dc11ed3addbeb14275f0c3
Mayur <m.joynauth@gmail.com>
2015-12-14 16:11:13 +0400

    Yields Sample

---
+++ YieldsSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldsSample
+{
+    class Program
+    {
+        static List<int> MyList = new List<int>();
+
+        static void FillValues()
+        {
+            MyList.Add(1);
+            MyList.Add(2);
+            MyList.Add(3);
+            MyList.Add(4);
+            MyList.Add(5);
+        }
+
+        static IEnumerable<int> FilterWithYield()
+        {
+            foreach (int i in MyList)
+            {
+                if (i > 3) yield return i;
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            FillValues();
+           
+            foreach (int i in FilterWithYield()) // Browses through the list
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
